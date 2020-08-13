2b381928b24134ea05f7bd7a8bf11880e0006310
Casey Elsasser <caseyelsasser@gmail.com>
2020-08-13 18:02:13 -0500

    Add files via upload

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,3 +1,6 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
 
@@ -13,5 +16,5 @@ CREATE TABLE post (
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  FOREIGN KEY (autho_id) REFERENCES user (id)
+  FOREIGN KEY (author_id) REFERENCES user (id)
 );
