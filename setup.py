3f5c44703ede8165154e9f3be716ef89502b399d
Mark Fowler <fowmar01@CSGICORP.COM>
2020-08-23 11:24:16 -0500

    remove unnecessary fields in setup file

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ with io.open("README.rst", "rt", encoding="utf8") as f:
 setup(
     name="flaskr",
     version="1.0.0",
-    url="http://flask.pocoo.org/docs/tutorial/",
-    license="BSD",
-    maintainer="Pallets team",
-    maintainer_email="contact@palletsprojects.com",
-    description="The basic blog app built in the Flask tutorial.",
-    long_description=readme,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
